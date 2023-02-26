e93bdc1fcc8eec27cbc25f792ca375f625eb147d
Erick Vasquez <erickmurillo0807@gmail.com>
2023-02-26 11:59:10 -0600

    add: yarn

--- linux/linux.sh
+++ linux/linux.sh
@@ -46,6 +46,13 @@ for app in "${app[@]}"; do
 done
 echo
 
+echo "Installing yarn"
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update
+sudo apt-get install yarn -y
+echo
+
 
 echo "Installing chrome"
 cd ~/Downloads
