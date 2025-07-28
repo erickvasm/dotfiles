a53a4225b9915d3fa1ca56ab89279660ef13f68d
Erick Vasquez <erickmurillo0807@gmail.com>
2025-07-27 18:22:57 -0600

    feat: update alias, nvim. vscode

--- os/unix/packages/nvim/.config/nvim/lua/config/keymaps.lua
+++ os/unix/packages/nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,9 +4,15 @@
 -- buffers navigation with tab
 vim.api.nvim_set_keymap("n", "<TAB>", ":bnext<CR>", { noremap = true, silent = true })
 
--- ALL TEXT, PASTE, RENDO, CUT
+-- ALL TEXT, PASTE, RENDO, CUT. Ctr r -> rendo
 vim.api.nvim_set_keymap("n", "<C-a>", "ggVG", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-v>", '"+p', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-z>", "u", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-x>", '"+x', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-d>", ":bd<CR>", { noremap = true, silent = true })
+
+-- Mover línea arriba en modo normal con Option + Shift + Up (Alt+Shift+Up)
+vim.api.nvim_set_keymap("n", "<A-S-Up>", ":m .-2<CR>==", { noremap = true, silent = true })
+
+-- Mover línea abajo en modo normal con Option + Shift + Down (Alt+Shift+Down)
+vim.api.nvim_set_keymap("n", "<A-S-Down>", ":m .+1<CR>==", { noremap = true, silent = true })
